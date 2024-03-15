e4fc4708e00916f2deb686eb6c94df8c0ab1b78c
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-03-15 13:40:41 +0100

    crear clase abstracta mesa

---
+++ src/main/java/Ejercicio1/Mesa.java
@@ -0,0 +1,15 @@
+package Ejercicio1;
+
+import java.time.LocalDate;
+
+abstract class Mesa {
+    protected int numeroIntegrantes;
+    protected String ubicacion;
+    protected LocalDate ultimaReserva;
+
+    public Mesa(int numeroIntegrantes, String ubicacion, LocalDate ultimaReserva) {
+        this.numeroIntegrantes = numeroIntegrantes;
+        this.ubicacion = ubicacion;
+        this.ultimaReserva = ultimaReserva;
+    }
+}
