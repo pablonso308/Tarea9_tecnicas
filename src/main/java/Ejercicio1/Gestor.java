b2753895c1aa03f5888816795477d5ab44ab153f
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-03-15 17:30:06 +0100

    metodo pedir mesa no funciona

--- src/main/java/Ejercicio1/Gestor.java
+++ src/main/java/Ejercicio1/Gestor.java
@@ -1,6 +1,8 @@
 package Ejercicio1;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Gestor {
     private ArrayList<Mesa> mesas;
@@ -20,6 +22,40 @@ public class Gestor {
         }
     }
 
+    public void pedirMesa() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el tipo de mesa que desea (Individual/Doble/Grupo): ");
+        String tipoMesa = scanner.nextLine();
+        System.out.println("Ingrese el número de la mesa: ");
+        int numero = Integer.parseInt(scanner.nextLine());
+        System.out.println("Ingrese la ubicación de la mesa (interior/terraza): ");
+        String ubicacion = scanner.nextLine();
+        LocalDate ultimaReserva = LocalDate.now();
+
+        switch (tipoMesa.toLowerCase()) {
+            case "individual":
+                System.out.println("Ingrese la vista de la mesa (jardín/calle): ");
+                String vistaIndividual = scanner.nextLine();
+                MesaIndividual mesaIndividual = new MesaIndividual(numero, ubicacion, ultimaReserva, vistaIndividual);
+                agregarMesa(mesaIndividual);
+                break;
+            case "doble":
+                System.out.println("Ingrese la vista de la mesa (jardín/calle): ");
+                String vistaDoble = scanner.nextLine();
+                MesaDoble mesaDoble = new MesaDoble(numero, ubicacion, ultimaReserva, vistaDoble);
+                agregarMesa(mesaDoble);
+                break;
+            case "grupo":
+                System.out.println("Ingrese el número de enchufes de la mesa: ");
+                int numeroEnchufes = Integer.parseInt(scanner.nextLine());
+                MesaGrupo mesaGrupo = new MesaGrupo(numero, ubicacion, ultimaReserva, numeroEnchufes);
+                agregarMesa(mesaGrupo);
+                break;
+            default:
+                System.out.println("Tipo de mesa no válido.");
+                break;
+        }
+    }
 
 }
 
