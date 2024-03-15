b3a7899dcbb77d898a6ff152b04882e1d165a394
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-03-15 16:20:05 +0100

    main que no imprime nada

--- src/main/java/Ejercicio1/Main.java
+++ src/main/java/Ejercicio1/Main.java
@@ -1,4 +1,16 @@
 package Ejercicio1;
 
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args) {
+        MesaIndividual mesaIndividual = new MesaIndividual(5, "interior", LocalDate.now(), "jardín");
+        MesaDoble mesaDoble = new MesaDoble(10, "terraza", LocalDate.now(), "calle");
+        MesaGrupo mesaGrupo = new MesaGrupo(20, "interior", LocalDate.now(), 6);
+
+        System.out.println("Estas son las mesas que ofrecemos para reservar:");
+        mesaIndividual.mostrarInformacion();
+        mesaDoble.mostrarInformacion();
+        mesaGrupo.mostrarInformacion();
+    }
 }
