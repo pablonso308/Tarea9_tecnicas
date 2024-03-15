90c39a99e51f00719d1fc263344710f32bee641f
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-03-15 16:34:58 +0100

    crear nueva clase encargada de poder hacer reservas

---
+++ src/main/java/Ejercicio1/Gestor.java
@@ -0,0 +1,15 @@
+package Ejercicio1;
+
+import java.util.ArrayList;
+
+public class Gestor {
+    private ArrayList<Mesa> mesas;
+
+    public Gestor() {
+        this.mesas = new ArrayList<>();
+    }
+
+
+}
+
+
